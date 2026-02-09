2aeca05cfaf993ae526f986918e19d8927311311
Alexander Markevich <alexmarkevich1996@gmail.com>
2026-02-09 14:00:21 +0300

    feat: add readiness check before program execution

--- Genius_Idiot_Console_App/Program.cs
+++ Genius_Idiot_Console_App/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         ShowTestingRules();
+        PrepareBeforeTesting();
     }
     
     
@@ -15,5 +16,20 @@ class Program
                           "По каждому из вопросов вы должно ввести ответ в течении 10 секунд. \n" +
                           "Если вы не успеете ввести ответ, то система не засчитает вам баллы за вопрос и вы перейдете к следующему. \n" +
                           "По окончанию всех вопросов программа оценит количество правильных ответов и по ним определит уровень вашей эрудиции. Удачи вам!");
+        Console.WriteLine();
+    }
+    
+    static void PrepareBeforeTesting()
+    {
+        Console.WriteLine("Введите ключевое слово “Ready”, если поняли и приняли правила тестирования. \n" +
+                          "Тестирование не начнется, покуда вы не введете ключевое слово. ");
+
+        while (true)
+        {
+            string keyword = Console.ReadLine().ToLower();
+            if (keyword == "ready")
+                break;
+            Console.WriteLine("Неверное ключевое слово. Повторите, пожалуйста, попытку.");
+        }
     }
 }
