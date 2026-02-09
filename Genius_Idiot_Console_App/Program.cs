1629585458f702718c813dcb2f84116c98102838
Alexander Markevich <alexmarkevich1996@gmail.com>
2026-02-09 14:52:53 +0300

    feat: implement simple version of erudion test with score caclulation

--- Genius_Idiot_Console_App/Program.cs
+++ Genius_Idiot_Console_App/Program.cs
@@ -6,6 +6,7 @@ class Program
     {
         ShowTestingRules();
         PrepareBeforeTesting();
+        int finalScore = TestErudition();
     }
     
     
@@ -18,7 +19,6 @@ class Program
                           "По окончанию всех вопросов программа оценит количество правильных ответов и по ним определит уровень вашей эрудиции. Удачи вам!");
         Console.WriteLine();
     }
-    
     static void PrepareBeforeTesting()
     {
         Console.WriteLine("Введите ключевое слово “Ready”, если поняли и приняли правила тестирования. \n" +
@@ -31,5 +31,40 @@ class Program
                 break;
             Console.WriteLine("Неверное ключевое слово. Повторите, пожалуйста, попытку.");
         }
+    }
+    
+    static int TestErudition()
+    {
+        int score = 0;
+        Dictionary<string, string> questions = new Dictionary<string, string>()
+        {
+            {"Сколько будет два плюс два умноженное на два?", "6"},
+            {"Бревно нужно распилить на 10 частей, сколько надо сделать распилов", "9"},
+            {"“На двух руках 10 пальцев. Сколько пальцев на 5 руках”", "25"},
+            {"Укол делают каждые полчаса, сколько нужно минут для трех уколов?", "60"},
+            {"Пять свечей горело, две потухли. Сколько свечей осталось", "5"},
+        };
+
+        int currentQuestion = 1;
+        foreach (var (question, answer) in questions)
+        {
+            Console.WriteLine(question);
+            Console.Write("Ваш ответ: ");
+            var input = Console.ReadLine();
+
+            if (input == answer)
+            {
+                score++;
+                Console.WriteLine($"Вы ответили на вопрос #{currentQuestion}. Переходим к следующему.");
+            }
+            else if (input == null)
+                Console.WriteLine($"Вы не успели ответить на вопрос #{currentQuestion}. Ответ не засчитан");
+            else
+                Console.WriteLine($"Вы ответили на вопрос #{currentQuestion}. Переходим к следующему.");
+            currentQuestion++;
+            Console.WriteLine();
+        }
+
+        return score;
     }
 }
